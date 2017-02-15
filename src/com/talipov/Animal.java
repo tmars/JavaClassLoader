59d6a463bc871bd95ca24f9fb662a946a52ddfcb
Marsel <t.mars@mail.ru>
2017-02-15 10:33:23 +0300

    init

---
+++ src/com/talipov/Animal.java
@@ -0,0 +1,21 @@
+package com.talipov;
+
+/**
+ * Created by Марсель on 15.02.2017.
+ */
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
